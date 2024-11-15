294dfc9e1eac27e90b0c04ffc2f59db4410cfdb3
Kat Fairbanks <ktornwall@gmail.com>
2024-11-14 21:42:47 -0500

    Update base for personal GH pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), Info()],
-  base: "./",
+  base: "/yjs-inspector/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
